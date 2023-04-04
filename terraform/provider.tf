a5f6c23cf65d78e69a995241b235aa682ea77702
swetha611 <122018772+swetha611@users.noreply.github.com>
2023-04-04 19:47:40 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIAWX7O5DQYI6G4QZE4"
-secret_key = "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"
+access_key = "AKIAWX7O5DQYAKIXCE5H"
+secret_key = "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"
 }
